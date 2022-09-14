67cf8e0ba70cb9aadb024984d5582157bcf13667
elnurxo <elnurykh@code.edu.az>
2022-09-14 12:34:04 +0400

    Adding alt prop to img tag and rel to a tag

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -19,7 +19,7 @@ function Contact() {
         </div>
         {/* Image of location/map */}
         <div className="w3-container">
-          <img src="https://www.w3schools.com/w3images/map.jpg" className="w3-image" style={{width: '100%'}} />
+          <img src="https://www.w3schools.com/w3images/map.jpg" alt='map' className="w3-image" style={{width: '100%'}} />
         </div>
       </div>
     </>
